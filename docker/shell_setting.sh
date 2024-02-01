9f53efb7cbc79bb56161925b3e3d7011b0244313
OccasionallyNLP <111713221+OccasionallyNLP@users.noreply.github.com>
2024-02-02 07:03:01 +0900

    Create shell_setting.sh

---
+++ docker/shell_setting.sh
@@ -0,0 +1,4 @@
+# bash completion - 화살표 방향으로 자동완성
+apt-get install bash-completion
+
+# vim/etc/profile에 코드 추가 if [ -f/etc/bash_completion ]; then ./etc/bash_completion fi # 적용 source/etc/profile 실행
